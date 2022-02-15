2dbacd5375d2caf24eca1d8ddf177dbfafec3d10
Joseph Santos <josephsantos024@gmail.com>
2022-02-14 19:19:49 -0600

    update2

--- app-tony-and-joey-phase-2/src/App.js
+++ app-tony-and-joey-phase-2/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import ComCon from './components/ComCon';
 import TokenCon from './components/TokenCon';
 import ComForm from './components/ComForm';
-import Token from './components/Token'
+
 
 function App() {
+
+  const [cryptoArray, setCryptoArray] = useState([])
+
+  useEffect (() => {
+    fetch ('http://localhost:6001/coins')
+    .then(res=>res.json())
+    .then(setCryptoArray)
+  }, [])
+
   return (
     <div>
-      <Token/>
       <ComCon />
-      <TokenCon />
+      <TokenCon  cryptoArray={cryptoArray} />
       <ComForm />
     <h1>HI TONY</h1>
     </div>
