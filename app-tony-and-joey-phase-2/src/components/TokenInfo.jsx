6ce5d0d8d25195eba9fb9ad37f008293be0ee8b1
Joseph Santos <josephsantos024@gmail.com>
2022-02-17 16:13:31 -0600

    the

--- app-tony-and-joey-phase-2/src/components/TokenInfo.jsx
+++ app-tony-and-joey-phase-2/src/components/TokenInfo.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-function TokenInfo({Definition, Exchanges, Image, Year,handleClick}) {
+function TokenInfo({Definition, Exchanges, Image, Year, handleToken}) {
   const [toggleLike, setToggleLike] = useState(true)
 
   function handleToggle(){
@@ -8,19 +8,19 @@ function TokenInfo({Definition, Exchanges, Image, Year,handleClick}) {
   }
 
   return (
-   <div className="info-container">
-        {toggleLike ? (
-          <>
-            <button onClick={handleToggle} className="tokenInfoActive">Info </button>
-            <div> {Definition}</div>
-          </>
-        ) : (
-          <>
-            <button onClick={handleToggle} className="tokenInfo">Exchanges</button>
-            <div> {Exchanges} </div>
-          </>
-        )}
-      </div>
+   <><div className="info-container">
+      {toggleLike ? (
+        <>
+          <button onClick={handleToggle} className="tokenInfoActive">Info </button>
+          <div> {Definition}</div>
+        </>
+      ) : (
+        <>
+          <button onClick={handleToggle} className="tokenInfo">Exchanges</button>
+          <div> {Exchanges} </div>
+        </>
+      )}
+    </div><img src={'https://media2.giphy.com/media/n59dQcO9yaaaY/giphy.gif?cid=790b761137b5dc381718c646e373c6c0e62ac6a21cd34f6b&rid=giphy.gif&ct=g'} alt="loading..." /></>
   )
 }
 
