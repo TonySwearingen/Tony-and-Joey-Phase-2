f8d986b8419a6c2e3e771c3896e3fa7ff05f39bc
unlimited1974@aol.com <unlimited1974@aol.com>
2022-02-17 16:26:27 -0600

    Final Push

--- app-tony-and-joey-phase-2/src/components/TokenInfo.jsx
+++ app-tony-and-joey-phase-2/src/components/TokenInfo.jsx
@@ -8,20 +8,22 @@ function TokenInfo({Definition, Exchanges, Image, Year, handleToken}) {
   }
 
   return (
-   <><div className="info-container">
-      {toggleLike ? (
-        <>
-          <button onClick={handleToggle} className="tokenInfoActive">Info </button>
-          <div> {Definition}</div>
-        </>
-      ) : (
-        <>
-          <button onClick={handleToggle} className="tokenInfo">Exchanges</button>
-          <div> {Exchanges} </div>
-        </>
-      )}
-    </div><img src={'https://media2.giphy.com/media/n59dQcO9yaaaY/giphy.gif?cid=790b761137b5dc381718c646e373c6c0e62ac6a21cd34f6b&rid=giphy.gif&ct=g'} alt="loading..." /></>
+   <>
+      <div className="info-container">
+        {toggleLike ? (
+          <>
+            <button onClick={handleToggle} className="tokenInfoActive">Info </button>
+            <div> {Definition}</div>
+          </>
+        ) : (
+          <>
+            <button onClick={handleToggle} className="tokenInfo">Exchanges</button>
+            <div> {Exchanges} </div>
+          </>
+        )}
+      </div><img src={'https://media2.giphy.com/media/n59dQcO9yaaaY/giphy.gif?cid=790b761137b5dc381718c646e373c6c0e62ac6a21cd34f6b&rid=giphy.gif&ct=g'} alt="loading..." />
+    </>
   )
 }
 
-export default TokenInfo
+export default TokenInfo;
